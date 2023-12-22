8aca25847fef15ab13cfaae612fa0f2f4e04f555
hackiku <ivan@pipewriter.io>
2023-12-22 01:07:40 +0100

    add date, source, score data points

--- src/lib/NewsCard.svelte
+++ src/lib/NewsCard.svelte
@@ -4,21 +4,54 @@
   export let longDescription;
   export let link;
   export let image;
+  export let date; // new prop for date
+  export let time; // new prop for time
+  export let source; // new prop for source
+  export let fomoScore; // new prop for FOMO score
 
   let isOpen = false;
 
   function toggleOpen() {
     isOpen = !isOpen;
   }
+
+  function handleKeydown(event) {
+    if (event.key === 'Enter') {
+      toggleOpen();
+    }
+  }
+
 </script>
 
-<div class="news-card bg-gray-800 p-6 mb-4 rounded-lg cursor-pointer" on:click={toggleOpen}>
-  <h2 class="text-xl text-white font-bold mb-2">{title}</h2>
-  {#if !isOpen}
-    <p class="text-gray-300">{shortDescription}</p>
-  {:else}
-    <p class="text-gray-300 mb-4">{longDescription}</p>
-    <a href={link} class="text-blue-400 hover:text-blue-600 mb-4 inline-block">Read More</a>
-    <img src={image} alt={title} class="w-48 h-48 object-cover"/>
-  {/if}
+<div 
+  class="news-card bg-gray-800 p-6 mb-4 rounded-lg cursor-pointer flex" 
+  on:click={toggleOpen}
+  role="button" 
+  tabindex="0"
+  on:keydown={handleKeydown}>
+
+  <div class="flex-1">
+    <h2 class="text-xl text-white font-bold mb-2">{title}</h2>
+    {#if !isOpen}
+      <p class="text-gray-300">{shortDescription}</p>
+    {:else}
+      <p class="text-gray-300 mb-4">{longDescription}</p>
+      <a href={link} class="text-blue-400 hover:text-blue-600 mb-4 inline-block">Read More</a>
+      <!-- Image Column (only when open) -->
+      <div class="mt-4">
+        <img src={image} alt={title} class="w-48 h-48 object-cover rounded"/>
+      </div>
+    {/if}
+    <div class="flex justify-between text-sm text-gray-400 mt-2">
+      <span>{date}</span>
+      <span>{time}</span>
+      <span>Source: {source}</span>
+      <span>FOMO Score: {fomoScore}</span>
+    </div>
+  </div>
+
+  <!-- Image Column -->
+  <div class="flex-none ml-4">
+    <img src={image} alt={title} class="w-48 h-48 object-cover rounded"/>
+  </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,30 @@
 <script>
-  // Placeholder for news items (you can replace this with your actual data fetching logic)
+  import { onMount } from 'svelte';
+  import NewsCard from '$lib/NewsCard.svelte';
+
   let newsItems = [
-    { title: "Mars Rover Discovers New Evidence of Water", summary: "NASA's Mars Rover has made a groundbreaking discovery on the Red Planet – traces of historical water flow. This revelation is not just about water; it hints at the possibility of life in Mars' past. The Rover's findings add a significant piece to the puzzle of Mars' environmental history, opening up new avenues for research and exploration. Scientists are now keenly analyzing this data, as it could reshape our understanding of the Martian landscape and its capacity to have supported life. This discovery propels us closer to answering the age-old question: Are we alone in the universe?" },
-    { title: "SpaceX Announces New Lunar Mission", summary: "SpaceX has announced a new mission to the moon, scheduled for late 2023, aiming to establish a sustainable human presence." },
-    { title: "SpaceX Announces New Lunar Mission", summary: "Hold on to your space helmets, fellow star gazers! SpaceX is gearing up for a monumental leap – a brand spanking new mission to the Moon, set for late 2023. This isn't just any lunar jaunt; it's the dawn of a new era! Imagine a sustainable human presence on the Moon, a base where astronauts can sip space coffee while gazing at Earth. The stakes? High. The excitement? Astronomical. If you've ever dreamt of moonwalking or felt that irresistible pull of the cosmos, this mission is a giant leap closer to that reality. Miss this, and you're missing a chapter of history. Get ready, space enthusiasts – the space race is on, and it's more thrilling than ever!"}
-
-    // ... more news items
+    {
+      title: 'Mars Rover Discovers New Evidence of Water',
+      shortDescription: 'NASA’s Mars Rover discovers traces of historical water flow on Mars.',
+      longDescription: 'NASA\'s Mars Rover has made a groundbreaking discovery on the Red Planet – traces of historical water flow. This revelation is not just about water; it hints at the possibility of life in Mars\' past. The Rover\'s findings add a significant piece to the puzzle of Mars\' environmental history, opening up new avenues for research and exploration. Scientists are now keenly analyzing this data, as it could reshape our understanding of the Martian landscape and its capacity to have supported life. This discovery propels us closer to answering the age-old question: Are we alone in the universe?',
+      link: '#',
+      image: 'mars-rover.jpg'
+    },
+    {
+      title: 'SpaceX Announces New Lunar Mission',
+      shortDescription: 'SpaceX sets its sights on the Moon with a new mission for late 2023.',
+      longDescription: 'Hold on to your space helmets, fellow star gazers! SpaceX is gearing up for a monumental leap – a brand spanking new mission to the Moon, set for late 2023. This isn\'t just any lunar jaunt; it\'s the dawn of a new era! Imagine a sustainable human presence on the Moon, a base where astronauts can sip space coffee while gazing at Earth. The stakes? High. The excitement? Astronomical. If you\'ve ever dreamt of moonwalking or felt that irresistible pull of the cosmos, this mission is a giant leap closer to that reality. Miss this, and you\'re missing a chapter of history. Get ready, space enthusiasts – the space race is on, and it\'s more thrilling than ever!',
+      link: '#',
+      image: 'spacex-moon.jpg'
+    },
+    {
+      title: 'Hubble Captures Breathtaking New Galaxy Image',
+      shortDescription: 'The Hubble Space Telescope reveals stunning new details of a distant galaxy.',
+      longDescription: 'In a remarkable feat of cosmic photography, the Hubble Space Telescope has captured a breathtaking image of a distant galaxy, revealing new details that were previously obscured. This new image provides astronomers with invaluable insights into the galaxy\'s structure, composition, and the mysteries of its luminous core. The clarity and detail of the image have left scientists and space enthusiasts alike in awe, offering a glimpse into the complexities of our universe and reminding us of the endless wonders yet to be discovered.',
+      link: '#',
+      image: 'hubble-galaxy.jpg'
+    }
   ];
-  
-
-
-  
-
-
-
 
 </script>
 
@@ -51,24 +62,18 @@
       <a href="#" class="text-gray-500 hover:bg-gray-300 px-4 py-2 rounded-full border border-gray-500">Breakthroughs</a>    </div>
   </section>
 
-
   <section>
     {#each newsItems as item}
-      <div class="news-item flex mb-6 bg-gray-800 shadow-xl rounded-xl overflow-hidden">
-        <!-- Text Column -->
-        <div class="text-column flex-1 p-6">
-          <h3 class="text-2xl text-white font-bold mb-2">{item.title}</h3>
-          <p class="text-gray-300">{item.summary}</p>
-          <a href="{item.link}" class="text-blue-400 hover:text-blue-600 mt-4 inline-block">Read More</a>
-        </div>
-
-        <!-- Image Column -->
-        <div class="image-column flex-none">
-          <img src="{item.image}" alt="{item.title}" class="w-48 h-48 object-cover"/>
-        </div>
-      </div>
+      <NewsCard
+        title={item.title}
+        shortDescription={item.shortDescription}
+        longDescription={item.longDescription}
+        link={item.link}
+        image={item.image} />
     {/each}
   </section>
+    
+  
 
   <section class="cta-section bg-gray-800 text-center p-6 rounded-lg mx-auto mt-8">
     <h2 class="text-3xl text-white font-bold mb-4">Call to Arms</h2>
