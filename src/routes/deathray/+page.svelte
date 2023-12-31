9f108507fde3da9eeb75642548cf9e75252bd399
hackiku <ivan@pipewriter.io>
2023-12-31 01:39:22 +0100

    lock to gray out input 1st implementation

--- src/routes/deathray/+page.svelte
+++ src/routes/deathray/+page.svelte
@@ -1,73 +1,110 @@
 <script>
-    let orbitalAltitude = 2000; // meters
-    let mirrorDiameter = 2.5; // meters
-    let focalPointSize = 0.05; // meters
-  
+    import { writable } from 'svelte/store';
+
+    // Creating writable stores
+    const orbitalAltitude = writable(2000); // meters
+    const mirrorDiameter = writable(2.5); // meters
+    const focalPointSize = writable(0.05); // meters
+    const desiredTemperature = writable(1000); // Kelvin
+    const heatFlux = writable(0);
+    const temperature = writable(0);
+
+    // Lock states
+    const lockOrbitalAltitude = writable(false);
+    const lockMirrorDiameter = writable(false);
+    const lockFocalPointSize = writable(false);
+    const lockTemperature = writable(false);
+
+    // Constants
     const SUN_INTENSITY = 1361; // Solar constant (W/m^2)
     const MIRROR_EFFICIENCY = 0.85; // Mirror's reflection efficiency
     const SPECIFIC_HEAT_CAPACITY = 550; // J/kg K for regolith
     const REGOLITH_DENSITY = 1500; // kg/m3 for regolith
     const BASE_TEMPERATURE = 250; // Average lunar surface temperature in Kelvin
-  
-    let heatFlux = 0;
-    let temperature = 0;
-  
-    function calculateMirrorDiameter(focalPointSize, orbitalAltitude) {
-      return focalPointSize * Math.sqrt(orbitalAltitude);
+
+    // Calculation functions
+    function calculateMirrorDiameter(focalSize, orbitalAlt) {
+        return focalSize * Math.sqrt(orbitalAlt);
     }
-  
-    function calculateHeatFlux(mirrorDiameter, focalPointSize) {
-      const mirrorArea = Math.PI * (mirrorDiameter / 2) ** 2;
-      const totalSolarPower = SUN_INTENSITY * mirrorArea * MIRROR_EFFICIENCY;
-      return totalSolarPower / (Math.PI * (focalPointSize / 2) ** 2);
+
+    function calculateHeatFlux(mirrorDia, focalSize) {
+        const mirrorArea = Math.PI * (mirrorDia / 2) ** 2;
+        const totalSolarPower = SUN_INTENSITY * mirrorArea * MIRROR_EFFICIENCY;
+        return totalSolarPower / (Math.PI * (focalSize / 2) ** 2);
     }
-  
-    function calculateTemperature(heatFlux) {
-      const focalPointArea = Math.PI * (focalPointSize / 2) ** 2;
-      const affectedVolume = focalPointArea * 0.01; // Assuming 1 cm depth
-      const regolithMass = affectedVolume * REGOLITH_DENSITY;
-      const energyAbsorbed = heatFlux * focalPointArea * 3600; // Exposure time = 3600 seconds
-      return BASE_TEMPERATURE + energyAbsorbed / (regolithMass * SPECIFIC_HEAT_CAPACITY);
+
+    function calculateTemperature(heatFl, focalSize) {
+        const focalPointArea = Math.PI * (focalSize / 2) ** 2;
+        const affectedVolume = focalPointArea * 0.01; // Assuming 1 cm depth
+        const regolithMass = affectedVolume * REGOLITH_DENSITY;
+        const energyAbsorbed = heatFl * focalPointArea * 3600; // Exposure time = 3600 seconds
+        return BASE_TEMPERATURE + energyAbsorbed / (regolithMass * SPECIFIC_HEAT_CAPACITY);
     }
-  
+
+    // Function to update calculations
     function updateCalculations() {
-      mirrorDiameter = calculateMirrorDiameter(focalPointSize, orbitalAltitude);
-      heatFlux = calculateHeatFlux(mirrorDiameter, focalPointSize);
-      temperature = calculateTemperature(heatFlux);
+        if (!$lockMirrorDiameter) {
+            mirrorDiameter.set(calculateMirrorDiameter($focalPointSize, $orbitalAltitude));
+        }
+        if (!$lockFocalPointSize) {
+            // Example logic: Inversely proportional to altitude, proportional to diameter
+            focalPointSize.set($mirrorDiameter / Math.sqrt($orbitalAltitude));
+        }
+        if (!$lockTemperature) {
+            temperature.set(calculateTemperature($heatFlux, $focalPointSize));
+        }
+        // Always update heat flux
+        heatFlux.set(calculateHeatFlux($mirrorDiameter, $focalPointSize));
     }
-  </script>
-    
-  <!-- <div class="bg-gray-800 p-8 m-32 rounded-3xl text-white"> -->
+
+    // Toggle lock function
+    function toggleLock(lockState) {
+        lockState.update(n => !n);
+    }
+</script>
 
 
-  <div class="bg-gray-800 p-8 m-32 rounded-3xl text-white">
-    <h1 class="text-2xl font-bold mb-4">Lunar Death Ray Orbiter Model</h1>
-    
-    <div class="mb-3">
-      <label for="orbitalAltitude" class="block text-lg mb-2">Orbital Altitude (meters):</label>
-      <input id="orbitalAltitude" type="number" bind:value={orbitalAltitude} on:input={updateCalculations}
-             class="w-full p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500">
+
+<div class="bg-gray-900 p-6 m-32 rounded-xl text-white">
+    <h1 class="text-3xl font-bold mb-6">Lunar Death Ray Orbiter Model</h1>
+
+    <!-- Orbit Section -->
+    <div class="bg-gray-800 p-4 rounded-lg mb-6">
+        <div class="flex justify-between items-center" class:opacity-60={$lockOrbitalAltitude}>
+            <label for="orbitalAltitudeSlider" class="block text-lg mb-2">
+                Orbital Altitude (0 - 300,000 meters):
+            </label>
+            <button on:click={() => toggleLock(lockOrbitalAltitude)}>
+                {@html $lockOrbitalAltitude ? '🔒' : '🔓'}
+            </button>
+        </div>
+        <input id="orbitalAltitudeSlider" type="range" min="0" max="300000" bind:value={$orbitalAltitude} 
+               class:opacity-60={$lockOrbitalAltitude} disabled={$lockOrbitalAltitude}
+               on:input={updateCalculations} class="slider w-full">
+        <p class="text-lg mt-2">Current Altitude: {$orbitalAltitude} meters</p>
     </div>
-    
-    <div class="mb-3">
-      <label for="mirrorDiameter" class="block text-lg mb-2">Mirror Diameter (meters):</label>
-      <input id="mirrorDiameter" type="number" bind:value={mirrorDiameter} on:input={updateCalculations}
-             class="w-full p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500">
+
+    <!-- Mirror Section -->
+    <div class="bg-gray-800 p-4 rounded-lg mb-6">
+        <div class="flex mb-3">
+            <div class="w-1/2 pr-2" class:opacity-60={$lockMirrorDiameter}>
+                <label for="mirrorDiameter" class="block text-lg mb-2">Mirror Diameter (meters):</label>
+                <input id="mirrorDiameter" type="number" bind:value={$mirrorDiameter} 
+                       class:opacity-60={$lockMirrorDiameter} disabled={$lockMirrorDiameter}
+                       on:input={updateCalculations} class="w-full p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500">
+            </div>
+            <div class="w-1/2 pl-2" class:opacity-60={$lockFocalPointSize}>
+                <label for="focalPointSize" class="block text-lg mb-2">Focal Point Size (meters):</label>
+                <input id="focalPointSize" type="number" bind:value={$focalPointSize} 
+                       class:opacity-60={$lockFocalPointSize} disabled={$lockFocalPointSize}
+                       on:input={updateCalculations} class="w-full p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500">
+            </div>
+        </div>        
     </div>
-    
-    <div class="mb-3">
-      <label for="focalPointSize" class="block text-lg mb-2">Focal Point Size (meters):</label>
-      <input id="focalPointSize" type="number" bind:value={focalPointSize} on:input={updateCalculations}
-             class="w-full p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500">
+
+    <!-- Power Section -->
+    <div class="bg-gray-800 p-4 rounded-lg">
+        <p class="text-lg">Heat Flux at Lunar Surface: {$heatFlux.toFixed(2)} W/m^2</p>
+        <p class="text-lg">Calculated Temperature at Focal Point: {$temperature.toFixed(2)} K</p>
     </div>
-    
-    <div class="mt-4">
-      <p class="text-lg">Heat Flux at Lunar Surface: {heatFlux.toFixed(2)} W/m^2</p>
-      <p class="text-lg">Calculated Temperature at Focal Point: {temperature.toFixed(2)} K</p>
-    </div>
-  </div>
-  
-  <style>
-    /* Add your styling here */
-  </style>
-  
+</div>
