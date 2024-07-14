f4ece202e89523d415778038d243a7ac64a2ad32
hackiku <ivan@pipewriter.io>
2024-07-14 17:32:04 +0200

    sat logo

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -6,7 +6,12 @@
 </script>
 
 <nav class="mt-4 mx-auto rounded-full px-12 py-2 border border-gray-500 flex justify-center items-center space-x-4 w-max">
-  <a href="/" class="text-lg font-bold">📡 SpaceFOMO</a>
+  <!-- <a href="/" class="text-lg font-bold">📡 SpaceFOMO</a> -->
+  <a href="/" class="text-lg font-bold flex mr-10">
+		<img src="logo.png" class="w-8 mr-2">
+		<div>
+		Spacefomo
+		</div></a>
   <a href="/feed" class="text-gray-400 hover:text-white text-lg">Feed</a>
   <Button variant="outline" class="text-lg">Start</Button>
   <ModeToggle />
