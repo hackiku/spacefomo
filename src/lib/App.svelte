35be37657eccab9e1dc47e11af4feb019dfcfa00
hackiku <ivan@pipewriter.io>
2025-01-09 19:29:24 +0100

    scaffold analytics section

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -3,15 +3,22 @@
 	import Hero from './components/Hero.svelte';
   import Fomo from './features/fomo/Fomo.svelte';
   import News from './features/news/News.svelte';
-	// import Analytics from './features/analytics/Analytics.svelte';
+	import Analytics from './features/analytics/Analytics.svelte';
 </script>
 
 <div class="relative overflow-hidden min-h-screen">
   <main class="relative z-10">
-    <Hero />
-    <Fomo />
-    <div class="bg-zinc-900/80">
-      <News />
-    </div>
+    <section>
+			<Hero />
+		</section>
+    <section>
+			<Fomo />
+		</section>
+    <section>
+			<News />
+		</section>
+    <section>
+			<Analytics />
+		</section>
   </main>
 </div>
