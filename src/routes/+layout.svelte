d0ed96d27935ab8ccda9fb30b510a442f09e47a9
hackiku <ivan@pipewriter.io>
2025-01-23 09:52:33 +0100

    import vercel analytics layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,15 @@
 <!-- src/routes/+layout.svelte -->
 <script lang="ts">
   import '../app.css';
+	import { dev } from '$app/environment';
 	import { ModeWatcher } from "mode-watcher";
   import Nav from '$lib/components/Nav.svelte';
   import Footer from '$lib/components/Footer.svelte';
   import SendNews from '$lib/components/cta/SendNews.svelte';
+
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 </script>
 
 <ModeWatcher />
