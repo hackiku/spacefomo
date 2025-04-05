9f8d9b13e8e6590023cc33507300aa48a3ab8c2a
hackiku <ivan@pipewriter.io>
2025-04-06 00:15:20 +0200

    deploy: add random about pg

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<!-- src/routes/about/+page.svelte -->
+<script lang="ts">
+  import About from '$lib/components/contact/About.svelte';
+</script>
+
+<div class="container mx-auto px-4 py-12">
+  <About />
+</div>
